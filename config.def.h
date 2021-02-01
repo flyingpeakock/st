cf248a9cc0841b14957ebdb08045a4cb71482d14
Philip Johansson <philipphuket@gmail.com>
2021-02-01 16:50:24 +0100

    more stuff

--- config.def.h
+++ config.def.h
@@ -213,8 +213,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-    { MODKEY,               XK_l,           copyrul,        {.i =  0} },
-    { MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"},
+    { MODKEY,               XK_l,           copyurl,        {.i =  0} },
+    { MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };
 
 /*

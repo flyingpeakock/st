7c63412c090668af75ac438d24886b12c4c6c90f
Philip Johansson <philipphuket@gmail.com>
2021-02-02 21:59:39 +0100

    Testing in progress ;)

--- config.def.h
+++ config.def.h
@@ -231,8 +231,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_k,           kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ TERMMOD,              XK_K,           kscrollup,      {.i = -1} },
-	{ TERMMOD,              XK_J,           kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
     { MODKEY,               XK_l,           copyurl,        {.i =  0} },
     { MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };

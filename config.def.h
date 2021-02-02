27d5aa902d7a38bb4208d7eb240929de05de872d
Philip Johansson <philipphuket@gmail.com>
2021-02-02 15:11:45 +0100

    stopped caring 23 commits ago

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Hack:size=9.5:antialias=true:autohint=true";
-static int borderpx = 0;
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
